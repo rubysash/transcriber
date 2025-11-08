e649793d4bcde3c66054b6890ebe59f625670e25
James Fraze <46380125+rubysash@users.noreply.github.com>
2025-11-08 16:32:32 -0600

    Add files via upload
    Fixed typo in logger, added screenshot image

--- logger.py
+++ logger.py
@@ -18,14 +18,26 @@ class Logger:
     def _write(self, msg, level="INFO"):
         """Internal write method"""
         # Import here to avoid circular import
-        from config import DEBUG_MODE
+        try:
+            from config import DEBUG_MODE
+        except ImportError:
+            DEBUG_MODE = True  # Default to verbose if config not available
         
         # Skip debug messages if DEBUG_MODE is off
         if level == "DEBUG" and not DEBUG_MODE:
+            # Still write to log file, just don't print to console
+            if self.log_file:
+                try:
+                    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    formatted = f"[{timestamp}] [{level}] {msg}"
+                    with open(self.log_file, 'a', encoding='utf-8') as f:
+                        f.write(formatted + '\n')
+                except:
+                    pass
             return
         
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        formatted = f"[{timestamp}] [{level}] {msg}":%M:%S")
+        formatted = f"[{timestamp}] [{level}] {msg}"
         
         # Print to console
         print(formatted)
